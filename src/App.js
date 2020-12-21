2b89df407952c3ec53d112c38c64b99951e56ef1
keithfilegar <keith.filegar@gmail.com>
2020-12-21 13:56:10 -0800

    app components identified and created, search successfully fetches and sets state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Search from './search/search'
+import Filters from './filters/filters'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showFilter: false,
+      showList: false,
+      searchTerm: "",
+      results: []
+    }
+  }
+
+  setShowFilter(show) {
+    this.setState({
+      showFilter: show
+    })
+  }
+
+  setShowList(show) {
+    this.setState({
+      showList: show
+    })
+  }
+
+  setSearchTerm(searchTerm) {
+    this.setState({
+      searchTerm: searchTerm
+    })
+  }
+
+  handleSearch(e) {
+    e.preventDefault();
+    const formattedSearchTerm = encodeURIComponent(this.state.searchTerm);
+    const url = "https://www.googleapis.com/books/v1/volumes?q=" + formattedSearchTerm;
+
+    fetch(url)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Something went wrong. Please try again later.');
+      }
+      return response;
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        results: data.items
+      })
+    })
+    .catch(err => {
+      this.setState({
+        error: err.message
+      })
+    })
+
+  }
+
+  render(){
+    const error = this.state.error
+      ? <div className="error">{this.state.error}</div>
+      : "";
+    const filter = this.state.showFilter
+      ? <Filters />
+      : "";
+
+    return (
+      <div className="App">
+        <h1>Google Book Search</h1>
+        <Search 
+        state={this.state}
+        setSearchTerm={searchTerm => this.setSearchTerm(searchTerm)}
+        setShowFilter={show => this.setShowFilter(show)}
+        setShowList={show => this.setShowList(show)}
+        handleSearch={e => this.handleSearch(e)}/>
+        {error}
+        {filter}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/search/search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './search.css'
+
+class Search extends React.Component {
+
+    render(){
+        return (
+            <form className="search__form" onSubmit={e => this.props.handleSearch(e)}>
+                <label htmlFor="search">Search:</label>
+                <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search an author or title"
+                value={this.props.state.searchTerm}
+                onChange={e => this.props.setSearchTerm(e.target.value)}/>
+            </form>
+        )
+    }
+}
+
+export default Search
